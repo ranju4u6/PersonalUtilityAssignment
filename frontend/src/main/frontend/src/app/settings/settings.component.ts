12c9d6776d256eeddb98a96c5673a952e0ade215
ranju4u6 <ranju4u6@gmail.com>
2019-03-28 08:05:06 +0530

    user settings changes

--- frontend/src/main/frontend/src/app/settings/settings.component.ts
+++ frontend/src/main/frontend/src/app/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { User } from '../user/user';
+import { IUserType } from '../user/usertype';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  public addUserForm: FormGroup;
+  public userList: User[];
+  public userTypes: IUserType[];
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
+    this.addUserForm = this.formBuilder.group({
+      userName: ["",[Validators.required, Validators.minLength(3)]],
+      passwordGroup: this.formBuilder.group({
+        password: ["", [Validators.required, Validators.maxLength(3)]],
+        confirmPassword:[""]
+      }, {validator: conformPassword}),
+      userTypeId:["", Validators.required]
+    });
+
+    this.userService.getAllUsers().subscribe(
+      data=>{
+        this.userList = data;
+      }, errorData=>{
+
+      }
+    );
+
+    this.userService.getAllUserTypes().subscribe(
+      data=>{
+        this.userTypes = data;
+      }, errorData=>{
+
+      }
+    );
   }
 
+}
+
+function conformPassword(control: AbstractControl):{[key:string]:boolean}|null{
+  const password = control.get("password");
+  const confirmPassword = control.get("confirmPassword");
+
+  if((password && confirmPassword) && (password.value === confirmPassword.value)){
+    return null;
+  }else{
+    return {"passwordValidationStatus":true};
+  }
 }
